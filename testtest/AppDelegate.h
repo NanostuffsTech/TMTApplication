5fe1241f7c2eb30787f6b7f2ddf5c9c18c9d937d
NanostuffsTech <nanostuffs@Nurbash-Abdulgafar-Mac-mini-4.local>
2013-10-11 12:54:06 +0530

    Initial Commit

---
+++ testtest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  testtest
+//
+//  Created by NanoStuffs on 11/10/13.
+//  Copyright (c) 2013 NanoStuffs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ testtest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testtest
+//
+//  Created by NanoStuffs on 11/10/13.
+//  Copyright (c) 2013 NanoStuffs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
